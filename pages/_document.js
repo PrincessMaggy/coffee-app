974f4eb42ede2a19925e6f8ab8a811b2605136d1
PrincessMaggy <edoziemagdalene@gmail.com>
2023-01-16 15:42:39 +0100

    ...

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel='preload'  href='/public/fonts/IBMPlexSans-Bold.ttf' as='font' crossOrigin='anonymous'></link>
+        <link rel='preload'  href='/public/fonts/IBMPlexSans-Regular.ttf' as='font' crossOrigin='anonymous'></link>
+        <link rel='preload'  href='/public/fonts/IBMPlexSans-SemiBold.ttf' as='font' crossOrigin='anonymous'></link>      
+      </Head>
       <body>
         <Main />
         <NextScript />
