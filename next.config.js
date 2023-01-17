a99aad5bcf2b74b6eb38453a97aaa44b208c1f7a
PrincessMaggy <edoziemagdalene@gmail.com>
2023-01-17 11:17:41 +0100

    comment out issues

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{
-    loader:'default',
-    path: '/_next/image',
-  },
+  // images:{
+  //   loader:'default',
+  //   path: '/_next/image',
+  // },
 };
 
 module.exports = nextConfig
