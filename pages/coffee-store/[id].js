4f5eeecf2f67828432c3beb801845d937153cdc4
PrincessMaggy <edoziemagdalene@gmail.com>
2023-01-18 16:43:53 +0100

    ...

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,7 +1,31 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-const CoffeeStore = () => {
+import coffeeStoresData from '../../data/coffee-stores.json'
+
+export async function getStaticProps({staticProps}) {
+  const params = staticProps.params;
+  return{
+    props:{
+      coffeeStore:coffeeStoresData.find((coffeeStore)=> {
+        return coffeeStore.id === params.id }),
+    },
+  }
+}
+
+export function getStaticPaths(){
+  return{
+    paths:[
+      {params:{id: "0"}},
+      {params:{id: "1"}},
+      {params:{id:"2"}}
+    ],
+    fallback: false
+  }
+}
+
+
+const CoffeeStore = (props) => {
   const router = useRouter();
 
   return (
