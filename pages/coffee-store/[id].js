47daa06863a16ad642c4499bedc4626ffe29462e
PrincessMaggy <edoziemagdalene@gmail.com>
2023-02-14 11:31:37 +0100

    fixed static paths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,26 +1,31 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
 
 import coffeeStoresData from '../../data/coffee-stores.json'
 
-export async function getStaticProps({staticProps}) {
+export async function getStaticProps(staticProps) {
   const params = staticProps.params;
+  console.log("params", params)
   return{
     props:{
       coffeeStore:coffeeStoresData.find((coffeeStore)=> {
-        return coffeeStore.id === params.id }),
+        return coffeeStore.id.toString() === params.id }),
     },
   }
 }
 
 export function getStaticPaths(){
+  const paths = coffeeStoresData.map(coffeeStore=>{
+    return{
+      params:{
+        id:coffeeStore.id.toString()
+      }
+    }
+  })
   return{
-    paths:[
-      {params:{id: "0"}},
-      {params:{id: "1"}},
-      {params:{id:"2"}}
-    ],
-    fallback: false
+    paths,
+    fallback: true
   }
 }
 
@@ -28,10 +33,21 @@ export function getStaticPaths(){
 const CoffeeStore = (props) => {
   const router = useRouter();
 
+  if(router.isFallback){
+    return <div>Loading...</div>
+  }
+  const {address, name, neighbourhood} = props.coffeeStore
+
   return (
     <div>
-      coffee {router.query.id}
+    <Head>
+      <title>{name}</title>
+    </Head>
       <Link href="/">Back to home</Link>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighbourhood}</p>
+
     </div>
   )
 }
