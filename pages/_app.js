a366996b1c31cfb4b671c95365055f87e25e63b2
PrincessMaggy <edoziemagdalene@gmail.com>
2023-07-30 12:20:26 +0100

    use reducers and providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,50 @@
-import '../styles/globals.css'
+import {createContext, useReducer} from 'react';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const StoreContext = createContext();
+
+const ACTION_TYPES = {
+    SET_LAT_LONG: 'SET_LAT_LONG',
+    SET_COFFEE_STORES: 'SET_COFFEE_STORES',
+};
+
+const storeReducer = (state, action) => {
+    switch (action) {
+        case ACTION_TYPES.SET_COFFEE_STORES: {
+            return {
+                ...state,
+                coffeeStores: action.payload.coffeeStores,
+            };
+        }
+        case ACTION_TYPES.SET_LAT_LONG: {
+            return {
+                ...state,
+                latLong: action.payload.latLong,
+            };
+        }
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`);
+    }
+};
+
+const StoreProvider = ({children}) => {
+    const initialState = {
+        latLong: '',
+        coffeeStores: [],
+    };
+    const [state, dispatch] = useReducer(storeReducer, initialState);
+
+    return (
+        <StoreContext.Provider value={{state, dispatch}}>
+            {children}
+        </StoreContext.Provider>
+    );
+};
+
+export default function App({Component, pageProps}) {
+    return (
+        <StoreProvider>
+            <Component {...pageProps} />{' '}
+        </StoreProvider>
+    );
 }
